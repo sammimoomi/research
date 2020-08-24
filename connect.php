83f51a1525f6733a05fe7d09f31b79f90958f951
sammimoomi <suriyasam26@gmail.com>
2020-08-24 08:49:39 +0700

    update

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "us-cdbr-east-02.cleardb.com";
-$username ="bdd9d54a7e1fe7";
-$password ="08d623a3";
-$database_name = "heroku_4a536962f8b2749";
+$host = "localhost";
+$username ="root";
+$password ="";
+$database_name = "research";
 $table = "research_tb";
 $conn= mysqli_connect($host,$username,$password,$database_name) or die("Error: " . mysqli_error($conn));
 mysqli_query($conn, "SET NAMES 'utf8' ");
